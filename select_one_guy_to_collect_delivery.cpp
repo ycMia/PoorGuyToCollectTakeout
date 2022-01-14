d92311c87e4c5cd850fceefbcecd09585efdf1fa
TianC Wang <thewater2016@qq.com>
2022-01-14 16:17:10 +0800

    first round code format

--- select_one_guy_to_collect_delivery.cpp
+++ select_one_guy_to_collect_delivery.cpp
@@ -1,34 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
 
-#include<iostream>
-#include<time.h>
-
-
-using namespace std;
-
-int a[4];
-
-int main()
+int main(void)
 {
+	//-- import namespace
+	using namespace std;
+
+	//-- variables
+	int a[4] = {0};
+	// int hCount = 0;
+
+	//-- hash
 	srand((int)time(NULL));
-	
-	for(int i=0;i<100;i++)
-		a[i] = 0;
-	
-	int hCount = 0;
-		
-	for(int i = 0; i<100 ;i++)
+	for (int i = 0; i < 100; i++)
 	{
-		int t = rand()%RAND_MAX%4 + 1;
-		a[t-1]++;
-		//cout<<t<<endl;
+		int t = rand() /* % RAND_MAX */ % 4;
+		a[t]++;
+		// cout << t << endl;
 	}
-	
-	cout<<"Jerry: "<<a[0];
-	cout<<"Haris: "<<a[1];
-	cout<<"Steven: "<<a[2];
-	cout<<"William: "<<a[3];
-	
+
+	//-- output
+	cout << "Jerry: " << a[0] << '\n';
+	cout << "Haris: " << a[1] << '\n';
+	cout << "Steven: " << a[2] << '\n';
+	cout << "William: " << a[3] << '\n';
 	return 0;
 }
